a033291f619cef5fc7b8ff6562c7d9e7199593ce
yelifu1988 <1584861058@qq.com>
2018-01-08 23:37:25 +0800

    Notes

--- js实现无延迟菜单效果/demo.js
+++ js实现无延迟菜单效果/demo.js
@@ -1,105 +1,104 @@
-$(document).ready(function(){
-	// alert("hello!");
-	var sub=$('#sub');
-	// console.log(sub);
-	var activeRow;
-	var activeMenu;
-	var timer;
-	var mouseInSub = false;
-	sub.on("mouseenter",function(e){
-		mouseInSub = true;
-	}).on('mouseleave',function(e){
-		mouseInSub = false;
-	})
+$(document).ready(function () {
+    // alert("hello!");
+    var sub = $('#sub');
+    // console.log(sub);
+    var activeRow;
+    var activeMenu;
+    var timer;
+    var mouseInSub = false;//标识鼠标是否在菜单里
+    sub.on("mouseenter", function (e) {
+        mouseInSub = true;//在菜单里
+    }).on('mouseleave', function (e) {
+        mouseInSub = false;//不在菜单里
+    })
 
-	var mouseTrack = []
-	var moveHanlder=function(e){
+    var mouseTrack = []	//追踪鼠标位置
+    var moveHanlder = function (e) {	//获取鼠标相对于页面的坐标
         mouseTrack.push({
-        	x: e.pageX,
-        	y: e.pageY
+            x: e.pageX,
+            y: e.pageY
         })
-
-        if(mouseTrack.length>3){
-        	mouseTrack.shift()
+        //因为我们只需要当前的位置和上一次的位置 所以 多余的直接删除
+        if (mouseTrack.length > 3) {
+            mouseTrack.shift()	//mouseTrack数组里只留了上一次和最后一次的位置信息
         }
-	}
+    }
 
 
-	$("#test")
-	.on('mouseenter',function(e){
-        sub.removeClass('none');
-        $(document).bind('mousemove',moveHanlder)
+    $("#test")
+        .on('mouseenter', function (e) {
+            sub.removeClass('none');
+            $(document).bind('mousemove', moveHanlder)//记录鼠标位置
 
-	})
-	.on('mouseleave',function(e){
-         console.log('fire mouse leave')
-		sub.addClass("none");
-		if (activeRow) {
-    
-		    //如果存在一级菜单激活
-		    activeRow.removeClass('active');
-		    //去除样式一级菜单的active样式
-		    activeRow = null;
-		   }
-		   if (activeMenu) {
-		    //如果存在二级菜单激活
-		    activeMenu.addClass('none')
-		    //给二级菜单添加none样式
-		    activeMenu = null
-		   }
+        })
+        .on('mouseleave', function (e) {
+            console.log('fire mouse leave')
+            sub.addClass("none");
+            if (activeRow) {
 
-           $(document).unbind('mousemove',moveHanlder)
+                //如果存在一级菜单激活
+                activeRow.removeClass('active');
+                //去除样式一级菜单的active样式
+                activeRow = null;
+            }
+            if (activeMenu) {
+                //如果存在二级菜单激活
+                activeMenu.addClass('none')
+                //给二级菜单添加none样式
+                activeMenu = null
+            }
 
-		  })
-	.on('mouseenter','li',function(e){
-           if(!activeRow){
-           	activeRow= $(e.target)
-           	activeRow.addClass('active')
-           	activeMenu=$('#'+activeRow.data('id'))
-           	activeMenu.removeClass('none')
-           	return
-           } 
+            $(document).unbind('mousemove', moveHanlder)//解绑，以免影响其他组件
 
-           //setTimeout设置延迟
-	       //debounce去抖技术
-	       if(timer){
-	       	clearTimeout(timer);
-	       }
+        })
+        .on('mouseenter', 'li', function (e) {
+            if (!activeRow) {
+                activeRow = $(e.target)
+                activeRow.addClass('active')
+                activeMenu = $('#' + activeRow.data('id'))
+                activeMenu.removeClass('none')
+                return
+            }
 
-	       var currMousePos = mouseTrack[mouseTrack.length-1]
-	       var leftCorner = mouseTrack[mouseTrack.length-2]
-           var delay = needDelay(sub,leftCorner,currMousePos)
-           
-           if(delay){
-           	timer =setTimeout(function(){
-           	if(mouseInSub){
-           		return
-           	}
-           activeRow.removeClass('active')
-           activeMenu.addClass('none')
+            //setTimeout设置延迟
+            //debounce去抖技术
+            if (timer) {//如果该元素定时器还没有执行就清除，不执行定时器里的内容
+                clearTimeout(timer);
+            }
 
-           activeRow= $(e.target)
-           activeRow.addClass('active')
-           activeMenu=$('#'+activeRow.data('id'))
-           activeMenu.removeClass('none')
-           timer=null
-           },300)
-           }
-           
-           else{
-           	var prevActiveRow = activeRow;
-           	var prevActiveMenu = activeMenu;
+            var currMousePos = mouseTrack[mouseTrack.length - 1]	//鼠标当前点的坐标  p点坐标（判断 p 是不是在三角形内）
+            var leftCorner = mouseTrack[mouseTrack.length - 2]		//上一次的鼠标坐标
+            var delay = needDelay(sub, leftCorner, currMousePos)
 
-           	activeRow = $(e.target)
-           	activeMenu = $('#'+ activeRow.data('id'))
+            if (delay) {	//如果在三角形内 ，需要延迟
+                timer = setTimeout(function () {
+                    if (mouseInSub) {//如果在菜单里，不处理直接返回
+                        return
+                    }
+                    activeRow.removeClass('active')
+                    activeMenu.addClass('none')
 
-           	prevActiveRow.removeClass('active')
-           	prevActiveMenu.addClass('none')
+                    activeRow = $(e.target)
+                    activeRow.addClass('active')
+                    activeMenu = $('#' + activeRow.data('id'))
+                    activeMenu.removeClass('none')
+                    timer = null
+                }, 300)
+            } else {
+                var prevActiveRow = activeRow;
+                var prevActiveMenu = activeMenu;
 
-           	activeRow.addClass('active')
-           	activeMenu.removeClass('none')
+                activeRow = $(e.target)
+                activeMenu = $('#' + activeRow.data('id'))
 
-           }
-           
-	})
+                prevActiveRow.removeClass('active')
+                prevActiveMenu.addClass('none')
+
+                activeRow.addClass('active')
+                activeMenu.removeClass('none')
+
+            }
+
+
+        })
 })
--- js实现无延迟菜单效果/function.js
+++ js实现无延迟菜单效果/function.js
@@ -1,43 +1,44 @@
 //二维向量差乘公式
-function sameSign(a,b){
-return (a ^ b)>=0;
+function sameSign(a, b) {	//判断 a 和 b 符号相同
+    return (a ^ b) >= 0;
 }
 
 
-function vector(a,b) {
-	return{
-	x:b.x-a.x,
-	y:b.y-a.y
-	}
+function vector(a, b) {//终点坐标减去起点的坐标----向量
+    return {
+        x: b.x - a.x,
+        y: b.y - a.y
+    }
 }
 
-function vectorProduct(v1,v2) {
-return v1.x * v2.y-v2.x * v1.y;
+function vectorProduct(v1, v2) {
+    return v1.x * v2.y - v2.x * v1.y;
 }
 
-function isPointInTrangle(p,a,b,c){
-var pa=vector(p,a);
-var pb=vector(p,b);
-var pc=vector(p,c);
+function isPointInTrangle(p, a, b, c) {
+    var pa = vector(p, a);
+    var pb = vector(p, b);
+    var pc = vector(p, c);
 
-var t1=vectorProduct(pa,pb);
-var t2=vectorProduct(pb,pc);
-var t3=vectorProduct(pc,pa);
+    var t1 = vectorProduct(pa, pb);
+    var t2 = vectorProduct(pb, pc);
+    var t3 = vectorProduct(pc, pa);
 // t1  t2 t3的结果符号相同，证明点在三角形内部
-return sameSign(t1,t2)&&sameSign(t2,t3);
-}
-function needDelay(elem,leftCorner,currMousePos){
-var offset=elem.offset();
-
-var topLeft={
-x:offset.left,
-y:offset.top
+    return sameSign(t1, t2) && sameSign(t2, t3);
 }
 
-var bottomLeft={
-x:offset.left,
-y:offset.top+elem.height()
-}
+function needDelay(elem, leftCorner, currMousePos) {
+    var offset = elem.offset();
 
-return isPointInTrangle(currMousePos,leftCorner,topLeft,bottomLeft);
+    var topLeft = {//上边缘坐标
+        x: offset.left,
+        y: offset.top
+    }
+
+    var bottomLeft = {//下边缘坐标
+        x: offset.left,
+        y: offset.top + elem.height()
+    }
+
+    return isPointInTrangle(currMousePos, leftCorner, topLeft, bottomLeft);
 }
